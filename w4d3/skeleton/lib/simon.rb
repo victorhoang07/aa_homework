bcbf6b3880317cdc4bf73c7c413676854eca625b
Victor Hoang <vikterhoang@gmail.com>
2022-06-14 19:54:58 -0400

    finished simon

---
+++ w4d3/skeleton/lib/simon.rb
@@ -0,0 +1,54 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize(sequence_length = 1, game_over = false)
+    @sequence_length = sequence_length
+    @game_over = game_over
+    @seq = []
+  end
+
+  def play
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    self.require_sequence
+    self.show_sequence
+    if @game_over == false
+      self.round_success_message 
+      @sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    add_random_color
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
